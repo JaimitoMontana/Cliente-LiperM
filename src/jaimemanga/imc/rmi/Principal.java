a6de18dd66d8d6cedb3e80552b5809a25d8bece3
JaimeMontana <jaime.manga@unicolombo.edu.co>
2024-09-06 19:16:32 -0500

    Cliente LipeRmi Imc

---
+++ src/jaimemanga/imc/rmi/Principal.java
@@ -0,0 +1,14 @@
+
+package jaimemanga.imc.rmi;
+
+import jaimemanga.imc.rmi.vistas.VentanaPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
